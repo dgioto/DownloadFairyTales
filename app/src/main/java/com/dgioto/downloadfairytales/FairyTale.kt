4c9d691fa7b3b5d6ef1859a59ed5dfe41723c898
dgioto <dgioto@gmail.com>
2024-08-08 12:19:22 +0300

    we create a button to add data to the Firebase and create a screen to get a list of data

---
+++ app/src/main/java/com/dgioto/downloadfairytales/FairyTale.kt
@@ -0,0 +1,17 @@
+package com.dgioto.downloadfairytales
+
+data class FairyTale(
+    val name: String = "",
+    val description: String = "",
+    val price: String = "",
+    val category: String = "",
+    val imageUrl: String = ""
+
+    /*
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+    val isFavorite: Boolean
+     */
+)
--- app/src/main/java/com/dgioto/downloadfairytales/MainActivity.kt
+++ app/src/main/java/com/dgioto/downloadfairytales/MainActivity.kt
@@ -3,13 +3,25 @@ package com.dgioto.downloadfairytales
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dgioto.downloadfairytales.ui.theme.DownloadFairyTalesTheme
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
@@ -17,40 +29,80 @@ import com.google.firebase.firestore.firestore
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val fs = Firebase.firestore
-        fs.collection("FairyTales")
-            .document()
-            .set(mapOf("name" to "My fav book"))
-
-
-
         setContent {
             DownloadFairyTalesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    // We get access to the fire base object
+    val fs = Firebase.firestore
+    val list = remember {
+        mutableStateOf(emptyList<FairyTale>())
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DownloadFairyTalesTheme {
-        Greeting("Android")
+    // Write FairyTail objects to the list and we constantly monitor updates
+    fs.collection("FairyTales").addSnapshotListener {snapShot, exception ->
+        list.value = snapShot?.toObjects(FairyTale::class.java) ?: emptyList()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.8f)
+        ) {
+            items(list.value) { fairyTale ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
+                ) {
+                    Text(
+                        text = fairyTale.name,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth().padding(15.dp)
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Button(modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+            onClick = {
+                // Adding a FairyTale object with attributes to the Fairy Tales collection
+                fs.collection("FairyTales")
+                    .document().set(
+                        FairyTale(
+                            "My Book",
+                            "111111111",
+                            "100",
+                            "fiction",
+                            "url"
+                        )
+                    )
+            }) {
+            // Button name
+            Text(text = "Add Fairy Tale")
+        }
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    DownloadFairyTalesTheme {
+//        Greeting("Android")
+//    }
+//}
